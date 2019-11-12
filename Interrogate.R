909c4c0c711369b14ab55dcf182f2965c61508a3
Conor O'Farrelly <ofarreco@ITSERV.SCSS.TCD.IE>
2019-11-12 15:46:11 +0000

    Interrogate GitHub

---
+++ Interrogate.R
@@ -0,0 +1,33 @@
+install.packages("jsonlite")
+library(jsonlite)
+install.packages("httpuv")
+library(httpuv)
+install.packages("httr")
+library(httr)
+
+# Can be github, linkedin etc depending on application
+oauth_endpoints("github")
+
+# Change based on what you 
+myapp <- oauth_app(appname = "Access_GitHub",
+                   key = "1ab488ceeadc6a9bc3a5",
+                   secret = "ee2e58fd88e104b16b90da3a4825bfe8c40d94b7")
+
+# Get OAuth credentials
+github_token <- oauth2.0_token(oauth_endpoints("github"), myapp)
+
+# Use API
+gtoken <- config(token = github_token)
+req <- GET("https://api.github.com/users/jtleek/repos", gtoken)
+
+# Take action on http error
+stop_for_status(req)
+
+# Extract content from a request
+json1 = content(req)
+
+# Convert to a data.frame
+gitDF = jsonlite::fromJSON(jsonlite::toJSON(json1))
+
+# Subset data.frame
+gitDF[gitDF$full_name == "jtleek/datasharing", "created_at"] 
