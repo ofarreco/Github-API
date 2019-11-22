09b847ad2811c807bb555094fa76144938659e6b
ofarreco <55790512+ofarreco@users.noreply.github.com>
2019-11-22 11:50:17 +0000

    Update Interrogate.R

--- Interrogate.R
+++ Interrogate.R
@@ -31,3 +31,54 @@ gitDF = jsonlite::fromJSON(jsonlite::toJSON(json1))
 
 # Subset data.frame
 gitDF[gitDF$full_name == "jtleek/datasharing", "created_at"] 
+install.packages("jsonlite")
+install.packages("httr")
+install.packages("httpuv")
+install.packages("ggplot2")
+install.packages("plotly")
+install.packages('devtools')
+library(plotly)
+library(jsonlite)
+library(httpuv)
+library(httr)
+library(devtools)
+library(magrittr)
+library(dplyr)
+library(plotly)
+
+#Choose github as the endpoint
+oauth_endpoints("github")
+
+
+#Enter my personal github details
+myApplication <- oauth_app(appname = "Rstudio",
+                           key = "467fb84caca2c6e217d2",
+                           secret = "29bfddb0d3f3bc73fae1fe006c5782705bea5b8e")
+
+
+#Set and get my github token 
+githubToken <- oauth2.0_token(oauth_endpoints("github"), myApplication)
+getToken <- config(token = githubToken)
+
+
+
+#Returns a dataframe with information on the Current Users Followers 
+getFollowers <- function(ofarreco)
+{
+  i <- 1
+  x <- 1
+  followersDF <- data_frame()
+  while(x!=0)
+  {
+    followers <- GET( paste0("https://api.github.com/users/", username, "/followers?per_page=100&page=", i),getToken)
+    followersContent <- content(followers)
+    currentFollowersDF <- lapply(followersContent, function(x) 
+    {
+      df <- data_frame(user = x$login, userID = x$id, followersURL = x$followers_url, followingURL = x$following_url)
+    }) %>% bind_rows()
+    i <- i+1
+    x <- length(followersContent)
+    followersDF <- rbind(followersDF, currentFollowersDF)
+  }
+  return (followersDF)
+}
